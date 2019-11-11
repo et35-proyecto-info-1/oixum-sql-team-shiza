79b272a3f5087219c7f7a0a22163133860729088
miguel marconi <michelmarchoni@gmail.com>
2019-11-11 00:10:42 -0300

    arreglo de horrores, salta styles

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -1,9 +1,17 @@
+drop database if exists test;
 CREATE DATABASE IF NOT EXISTS test;
+use test;
 
-CREATE TABLE IF NOT EXISTS test.estudiantes (
-	id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE estudiantes (
+	id INT AUTO_INCREMENT,
 	nombre varchar(100) NOT NULL,
 	comidaPreferida varchar(100) NULL,
 	fechaNacimiento DATE NOT NULL,
 	CONSTRAINT estudiantes_PK PRIMARY KEY (id)
 );
+
+INSERT INTO test.estudiantes VALUES
+  ("1",'Federico Aloi', 'Panqueques de arroz', '1991-10-30'),
+  ("",'Andrea Sierra Bueno', 'Puré de papas con huevo revuelto', '1985-08-30'),
+  ("",'Fernando Cazas', 'Hamburguesas de lentejas', '1963-02-12'),
+  ("",'Marta Lucero', 'Conejo a la cacerola', '1938-01-24');
